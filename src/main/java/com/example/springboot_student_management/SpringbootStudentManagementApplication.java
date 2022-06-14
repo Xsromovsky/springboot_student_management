bd530df301754ea7f7d15330bcb04e259e023b09
samuel <samuel14sromovsky@gmail.com>
2022-06-14 22:08:38 +0200

    initial commit

---
+++ src/main/java/com/example/springboot_student_management/SpringbootStudentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_student_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudentManagementApplication.class, args);
+    }
+
+}
